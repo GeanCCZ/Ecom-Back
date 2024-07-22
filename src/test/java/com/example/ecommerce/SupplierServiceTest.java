0d261e96bfb8f5951b742b9a511dd2defae736bf
Gean <gean.souza017a@gmail.com>
2024-07-22 10:52:06 -0300

    Test: Supplier test changes

--- src/test/java/com/example/ecommerce/SupplierServiceTest.java
+++ src/test/java/com/example/ecommerce/SupplierServiceTest.java
@@ -1,4 +1,33 @@
 package com.example.ecommerce;
 
+import com.example.ecommerce.domain.dto.SupplierDTO;
+import com.example.ecommerce.service.SupplierService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
 public class SupplierServiceTest {
+
+    @Autowired
+    private SupplierService supplierService;
+
+    @Test
+    public void testSupplierCreate(){
+        SupplierDTO newSupplier = new SupplierDTO(null, "Transistors Unlimited", "Transistors Unlimited LTDA", "We made transistors", "11110001/1",null, null,null);
+
+        SupplierDTO createdSupplier = this.supplierService.create(newSupplier);
+
+        assertEquals(newSupplier.trade_name(), createdSupplier.trade_name());
+        assertEquals(newSupplier.legal_name(), createdSupplier.legal_name());
+        assertEquals(newSupplier.description(), createdSupplier.description());
+        assertEquals(newSupplier.cnpj(), createdSupplier.cnpj());
+
+        assertEquals(newSupplier.stockList(), createdSupplier.stockList());
+        assertEquals(newSupplier.productList(), createdSupplier.productList());
+        assertEquals(newSupplier.addressList(), createdSupplier.addressList());
+
+    }
 }
