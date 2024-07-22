33fdde18775bd8a6c61366ae2a0287edf6a346c8
Gean <gean.souza017a@gmail.com>
2024-07-22 10:45:20 -0300

    Test: Stock test changes

--- src/test/java/com/example/ecommerce/StockServiceTest.java
+++ src/test/java/com/example/ecommerce/StockServiceTest.java
@@ -1,4 +1,47 @@
 package com.example.ecommerce;
 
+import com.example.ecommerce.domain.dto.StockDTO;
+import com.example.ecommerce.service.StockService;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest
 public class StockServiceTest {
+
+    @Autowired
+    private StockService stockService;
+
+    @Test
+    public void testStockCreate(){
+
+        StockDTO newStock = new StockDTO(null, 1, null, null);
+
+        StockDTO createdStock = this.stockService.create(newStock);
+
+        assertEquals(newStock.product(), createdStock.product());
+        assertEquals(newStock.quantity(), createdStock.quantity());
+        assertEquals(newStock.supplier(), createdStock.supplier());
+
+    }
+
+    @Test
+    public void testStockUpdate(){
+
+        StockDTO newStock = new StockDTO(null, 1, null, null);
+
+        StockDTO createdStock = this.stockService.create(newStock);
+
+        StockDTO updateStockDTO = new StockDTO(createdStock.id(), 3, null, null);
+
+        StockDTO updatedStock = this.stockService.update(createdStock.id(),updateStockDTO);
+
+        assertEquals(updateStockDTO.product(), updatedStock.product());
+        assertEquals(updateStockDTO.quantity(), updatedStock.quantity());
+        assertEquals(updateStockDTO.supplier(), updatedStock.supplier());
+
+    }
+
 }
